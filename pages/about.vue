adb1c9868573fe17155be0c1409add517f0e8e64
holiday <93828862+RaresAndrei0@users.noreply.github.com>
2024-07-01 14:34:10 +0300

    small changes to the about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>About Me</h1>
-    <p>I have to work on my description :P</p>
+    <p>Hi! My name is Rares and I am a second year student at the Faculty of Engineering in Foreign Languages in the Politehnica University of Bucharest.</p>
+    <p>I enjoy learning about programming, operating systems and the latest technology advancements.</p>
+    <p>I also enjoy electrical engineering, music and philosophy.</p>
+    <p>I am always seeking every opportunity to leverage my technical expertise and learn new things.</p>
+    <p>Feel free to message me on LinkedIn and I will make sure to respond :)</p>
   </div>
 </template>
